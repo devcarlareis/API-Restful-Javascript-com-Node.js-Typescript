0e62e40ec377462ddf774460b167eea50aa1c8d5
Carla Reis <carla.jusreis@gmail.com>
2021-06-01 16:05:15 -0400

    gerenciamento de perfil

---
+++ src/modules/users/routes/profile.routes.ts
@@ -0,0 +1,32 @@
+import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
+import isAuthenticated from '../../../shared/http/middlewares/isAuthenticated';
+import ProfileController from '../controllers/ProfileController';
+
+const profileRouter = Router();
+const profileContoller = new ProfileController();
+
+profileRouter.use(isAuthenticated);
+
+profileRouter.get('/', profileContoller.show);
+
+profileRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().required(),
+      old_password: Joi.string(),
+      password: Joi.string().optional(),
+      password_confimation: Joi.string()
+        .valid(Joi.ref('password'))
+        .when('password', {
+          is: Joi.exist(),
+          then: Joi.required(),
+        }),
+    },
+  }),
+  profileContoller.update,
+);
+
+export default profileRouter;
